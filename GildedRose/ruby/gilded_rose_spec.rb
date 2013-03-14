256f9880ff0c1445cc9be969ecec73f89fb01e26
David Ed Mellum <david@edmellum.com>
2013-03-14 10:21:15 +0100

    Add sell in test

--- GildedRose/ruby/gilded_rose_spec.rb
+++ GildedRose/ruby/gilded_rose_spec.rb
@@ -3,12 +3,25 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe '#update_quality' do
+    let(:items) do
+      [Item.new('foo', sell_in: 10, quality: 10)]
+    end
+
     it 'does not change the name' do
       name = 'foo'
       items = [Item.new(name, sell_in: 0, quality: 0)]
       GildedRose.new(items).update_quality()
       items[0].name.should == name
     end
+
+    it 'lowers the sell in values of all items' do
+      items.each do |item|
+        old = item.sell_in
+        GildedRose.new(items).update_quality()
+        item.sell_in.should < old
+      end
+    end
+
   end
 
 end
